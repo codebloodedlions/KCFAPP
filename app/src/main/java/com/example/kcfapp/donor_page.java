aec5750538231781349175813ab0c0fc32fa646a
Deonna <deonnaowens23@gmail.com>
2022-04-09 01:41:06 -0500

    Version 1.2

--- app/src/main/java/com/example/kcfapp/donor_page.java
+++ app/src/main/java/com/example/kcfapp/donor_page.java
@@ -13,41 +13,26 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class donor_page extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class donor_page extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor_page);
 
 
-        TextView nameTextView, serviceTextView, pubOrPrivTextView;
+        TextView nameTextView, serviceTextView, addressTextView;
 
         EditText nameEditText = findViewById(R.id.nameEditText);
+        EditText addressEditText = findViewById(R.id.addressEditText);
 
         CheckBox foodCheckBox = findViewById(R.id.foodCheckBox);
         CheckBox clothingCheckBox = findViewById(R.id.clothingCheckBox);
         CheckBox shelterCheckBox = findViewById(R.id.shelterCheckBox);
         CheckBox HealthCheckBox = findViewById(R.id.healthCheckBox);
 
-        Spinner pubOrPrivSpinner = findViewById(R.id.pubOrPrivSpinner);
-
         Button submitBtn = findViewById(R.id.submitBtn);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.classification, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        pubOrPrivSpinner.setAdapter(adapter);
-        pubOrPrivSpinner.setOnItemSelectedListener(this);
-    }
-
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
-        String text = parent.getItemAtPosition(i).toString();
-        //Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
+
 }
