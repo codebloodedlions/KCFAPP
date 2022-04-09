3cb8be4891fb7ec8c39a37a64d77074aafaec4dc
Deonna <deonnaowens23@gmail.com>
2022-04-09 00:31:17 -0500

    Version 1.0

--- app/src/main/java/com/example/kcfapp/MainActivity.java
+++ app/src/main/java/com/example/kcfapp/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView hugs = findViewById(R.id.HUGS);
+
         MaterialButton refugeeBtn = findViewById(R.id.refugeeBtn);
         MaterialButton donorBtn = findViewById(R.id.donorBtn);
 
