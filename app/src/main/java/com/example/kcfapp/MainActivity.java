51b94cdd84896482984f9983564f4708f250bef8
deonnao <deonnaowens3@yahoo.com>
2022-04-10 00:44:30 -0500

    Version 2.0

--- app/src/main/java/com/example/kcfapp/MainActivity.java
+++ app/src/main/java/com/example/kcfapp/MainActivity.java
@@ -16,11 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db_helper db = new db_helper(MainActivity.this);
+        //db.addNewLocation("test", "test2", 1, 0, 1, 1);
 
         MaterialButton refugeeBtn = findViewById(R.id.refugeeBtn);
         MaterialButton donorBtn = findViewById(R.id.donorBtn);
 
+        refugeeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, need.class));
+            }
+        });
+
         donorBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, donor_page.class));
---
+++ app/src/main/java/com/example/kcfapp/need.java
@@ -0,0 +1,76 @@
+package com.example.kcfapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class need extends AppCompatActivity implements View.OnClickListener {
+
+    Button food, clothing, shelter, healthcare;
+    db_helper donorDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_need);
+
+        food = findViewById(R.id.food);
+        clothing = findViewById(R.id.clothing);
+        shelter = findViewById(R.id.shelter);
+        healthcare = findViewById(R.id.healthcare);
+
+        donorDB = new db_helper(this);
+
+        food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                food.setSelected(!food.isSelected());
+                startActivity(new Intent(need.this, food.class));
+            }
+        });
+
+        clothing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                clothing.setSelected(!clothing.isSelected());
+                startActivity(new Intent(need.this, clothing.class));
+            }
+        });
+
+        shelter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                shelter.setSelected(!shelter.isSelected());
+                startActivity(new Intent(need.this, shelter.class));
+            }
+        });
+
+        healthcare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // set the color to relative layout
+                healthcare.setSelected(!healthcare.isSelected());
+                startActivity(new Intent(need.this, health.class));
+
+            }
+        });
+
+
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+
+}
